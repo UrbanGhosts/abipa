379d078d3667c4ab85fdecc6347097fadbd792b2
Roman Kolnogorov <47493909+UrbanGhosts@users.noreply.github.com>
2021-09-29 19:19:13 +0500

    Add files via upload

--- assets/js/loggin.js
+++ assets/js/loggin.js
@@ -2,20 +2,16 @@ $("#enter").on("click", function () {
 	
 	var name = $("#username").val();
 	var password = $("#password").val();
-	return;
-	window.console.log('name: ' + name + ', pass: ' + password);
-
 	
 	$.ajax({
 		url: 'assets/js/test.php',
-		type: 'POST',
+		type: 'GET',
 		cache: false,
 		headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
 		data: { 'name': name, 'password': password },
 		dataType: 'text',
 		beforeSend: function () {
 			$("#enter").prop('disabled', true);
-			window.console.log('block button');
         },
 		success: function (data) {
 			alert(data);
