c9df79c2297248601cf764e33f8eb62c83e2854e
Roman Kolnogorov <47493909+UrbanGhosts@users.noreply.github.com>
2021-09-30 04:05:54 +0500

    Add files via upload

--- assets/js/loggin.js
+++ assets/js/loggin.js
@@ -21,6 +21,7 @@ $("#enter").on("click", function () {
 			$("#enter").prop('disabled', false);
 			var href = window.location.href;
 			href = href.split("index")[0];
+			alert(href + "workpage.html");
 			window.location.href = href + "workpage.html";
 		},
 	});
